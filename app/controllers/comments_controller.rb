92f2e92e6cdfb8d9f0167826480066c38b4c60ea
Javier Ingold <ingold.javer@gmail.com>
2021-11-23 19:51:32 -0300

    comments form and controller update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,21 @@
 class CommentsController < ApplicationController
-  
+  before_action :authenticate_user!
+  def new
+    @comment = current_user.comments.new
+  end 
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(params[:post_id]), notice: "Thanks for your comment!"
+    else 
+      redirect_to post_path(params[:post_id]), notice: @comment.errors.full_messages.to_sentence
+    end 
+  end 
+
+  private
+
+  def comment_params
+    params.permit(:content).merge(post_id: params[:post_id])
+  end 
 end
