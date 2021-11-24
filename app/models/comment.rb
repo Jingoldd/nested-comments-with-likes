8d485d64122a75c15d634ab08a6021f0822898e0
Javier Ingold <ingold.javer@gmail.com>
2021-11-23 21:13:33 -0300

    nested comments db setup and associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
 
   belongs_to :post
   belongs_to :user
+
+  # Nested comments associations setup
+  belongs_to :parent, class_name: 'Comment', optional: true #'Parent' class don't exist, we have to specify the class name. Optional true for 'parent' comments
+  has_many :comments, foreign_key: :parent_id # We have to specify the column of the table to find the 'child' comments
 end
