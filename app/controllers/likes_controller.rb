7a3822c03a71037be3a2b0f613d830ff41076631
Javier Ingold <ingold.javer@gmail.com>
2021-11-26 00:44:21 -0300

    likes controller and views change to polymorphic

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,11 @@
 class LikesController < ApplicationController
+  # When making likes polymorphic, post becomes likeable
   before_action :authenticate_user!
 
   def create
     @like = current_user.likes.new(like_params)
     if @like.save
-      redirect_to post_path(params[:post_id]), notice: 'Post liked!'
+      redirect_to @like.likeable, notice: "Liked!"
     else
       flash[:notice] = @like.errors.full_messages.to_sentence
     end 
@@ -12,14 +13,14 @@ class LikesController < ApplicationController
 
   def destroy
     @like = current_user.likes.find(params[:id]) #current_user ensures a user can't delete another user likes
-    post = @like.post #ensures reference to the post
+    likeable = @like.likeable #ensures reference to the post
     @like.destroy
-    redirect_to post
+    redirect_to likeable
   end
 
   private
 
-  def like_params
-    params.permit(:post_id) #create method through current_user already sets user_id   
+  def like_params # When making likes polymorphic, likeable_type should be added
+    params.permit(:likeable_id, :likeable_type) #create method through current_user already sets user_id   
   end  
 end
